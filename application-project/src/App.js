549294d0f167b67592b94e2b2a1a1734b8bca255
Brian L. Dahlstrom <bldahlstrom@Brians-MacBook-Pro-2.local>
2023-12-14 08:44:28 -0600

    updated app and styling, building for tiles that link for delivery goal

--- application-project/src/App.js
+++ application-project/src/App.js
@@ -1,52 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import './App.css'; 
-import Recipe from './recipe'; 
-
+import Axios from "axios";
+import { useState } from "react";
+import "./App.css";
+import RecipeTile from "./recipe";
 
 function App() {
+  const [query, setquery] = useState("");
+  const [recipes, setrecipes] = useState([]);
+  const [healthLabel, sethealthLabel] = useState("alcohol-cocktail");
 
-const APP_ID = "0b2bdffa"; 
-const APP_KEY = "2dea1eb59336a72b9ab887befd011ef1"; 
+  const YOUR_APP_ID = `0b2bdffa`;
+  const YOUR_APP_KEY = "2dea1eb59336a72b9ab887befd011ef1";
 
-const [recipes, setRecipes] = useState([]); 
-const [search, setSearch] = useState(""); 
-const [query, setQuery] = useState(" "); 
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=${healthLabel}`;
 
+  const getRecipeInfo = async () => {
+    var result = await Axios.get(url);
+    setrecipes(result.data.hits);
+    console.log(result.data.hits);
+  };
 
-useEffect(() => { 
-	getRecipes(); 
-}, [query]) 
-const getRecipes = async () => { 
-	const response = await fetch 
-		(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`); 
-	const data = await response.json(); 
-	setRecipes(data.hits); 
-
-}; 
-const updateSearch = e => { 
-	setSearch(e.target.value); 
-}; 
   const onSubmit = (e) => {
     e.preventDefault();
     getRecipeInfo();
   };
-} 
 
-return (
-  <div>
-    <h1 onClick={getRecipeInfo}>Recipe Search Tool</h1>
-    <form onSubmit={onSubmit}>
-      <input
-        className="input"
-        type="text"
-        placeholder="Enter an ingredient"
-        autoComplete="Off"
-        value={query}
-        onChange={(e) => setquery(e.target.value)}
-      />
-      <input type="submit" value="Search" />
-      <select className="app__healthdropdown">
-        onChange={(e) => setHealth(e.target.value)}
+  return (
+    <div className="app">
+      <h1 onClick={getRecipeInfo}>Food Recipe Plaza 🍔</h1>
+      <form className="app__searchForm" onSubmit={onSubmit}>
+        <input
+          className="app__input"
+          type="text"
+          placeholder="enter ingredient"
+          autoComplete="Off"
+          value={query}
+          onChange={(e) => setquery(e.target.value)}
+        />
+        <input className="app__submit" type="submit" value="Search" />
+
+        <select className="app__sethealthLabels">
+          onChange={(e) => sethealthLabel(e.target.value)}
           <option 
             value="alcohol-cocktail"
             >
@@ -102,16 +95,18 @@ return (
             >
               vegetarian
           </option>
+          
+        </select>
 
-      </select>
-    </form>
+      </form>
 
-    <div className="apprecipes">
-    {recipes.map((recipe) => {
-            return <p>{recipe["recipe"]["label"]}</p>;
+      <div className="app__recipes">
+        {recipes.map((recipe) => {
+            return <RecipeTile recipe={recipe}/>;
+          })}
+      </div>
     </div>
-  </div>
-);
-} 
+  );
+}
 
-export default App; 
+export default App;
